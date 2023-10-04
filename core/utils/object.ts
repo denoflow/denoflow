db5ed309f7dda62c59b2f58737761a44739b4edb
Owen Young <theowenyoung@gmail.com>
2023-10-05 01:12:35 +0800

    update next

--- core/utils/object.ts
+++ core/utils/object.ts
@@ -4,27 +4,6 @@ export function isObject(obj: unknown): boolean {
     obj !== null;
 }
 
-// export function isClass(func: unknown): boolean {
-//   // Class constructor is also a function
-//   if (
-//     !(func && (func as Record<string, unknown>).constructor === Function) ||
-//     (func as Record<string, unknown>).prototype === undefined
-//   ) {
-//     console.log("xxx");
-
-//     return false;
-//   }
-
-//   // This is a class that extends other class
-//   if (Function.prototype !== Object.getPrototypeOf(func)) {
-//     return true;
-//   }
-//   console.log("xx2x");
-
-//   // Usually a function will only have 'constructor' in the prototype
-//   return Object.getOwnPropertyNames((func as Record<string, unknown>).prototype)
-//     .length > 1;
-// }
 export function isClass(v: unknown): boolean {
   return typeof v === "function" && /^\s*class\s+/.test(v.toString());
 }
